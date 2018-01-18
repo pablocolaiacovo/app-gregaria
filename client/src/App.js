eccd871e957fa7313bce01d8397e02a5aee18883
Pablo Colaiacovo <pablo.colaiacovo@gmail.com>
2018-01-18 17:29:35 -0300

    Ejemplo de firebase auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import firebase from "firebase";
+import "firebase/auth";
+import { FirebaseAuth } from "react-firebaseui";
+import "./firebaseui-overrides.global.css"; // Import globally.
+
+// Get the Firebase config from the auto generated file.
+const firebaseConfig = require("./firebase-config.json").result;
+
+// Instantiate a Firebase app.
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 class App extends Component {
+  uiConfig = {
+    signInFlow: "redirect",
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+      signInSuccess: (user) => {
+        console.log(user);
+        this.setState({ signedIn: true });
+        return false;
+      }
+    }
+  };
+
+  state = {
+    signedIn: false
+  };
+
+  /**
+   * @inheritDoc
+   */
   render() {
     return (
       <div className="App">
@@ -13,6 +45,15 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        {!this.state.signedIn && (
+          <div>
+            <FirebaseAuth
+              uiConfig={this.uiConfig}
+              firebaseAuth={firebaseApp.auth()}
+            />
+          </div>
+        )}
+        {this.state.signedIn && <div>You are now signed In!</div>}
       </div>
     );
   }
